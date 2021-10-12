e9e90cd6a658eab33d89485b1e802ea28144fbab
Brian Tipton <brian.p.tipton@gmail.com>
2021-10-12 12:36:00 -0500

    added bold functionality

--- src/LatexParser.jl
+++ src/LatexParser.jl
@@ -1,12 +1,22 @@
 module LatexParser
 using Markdown
 export tex_left, tex_center
-function tex_left(equation)
+function tex_left(equation::String, bold::Bool=false)
+    if bold
+        equation = "``\\boldsymbol{" * equation
+        equation =  equation * "}``" 
+        return Markdown.parse(equation)
+    end
     equation = "``" * equation
     equation =  equation * "``" 
     return Markdown.parse(equation)
 end
-function tex_center(equation)
+function tex_center(equation::String, bold::Bool=false)
+    if bold
+        equation = "\$\\boldsymbol{" * equation
+        equation =  equation * "}\$" 
+        return Markdown.parse(equation)
+    end
     equation = "\$" * equation
     equation =  equation * "\$" 
     return Markdown.parse(equation)
