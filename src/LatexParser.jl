99a558310a1da484ae204e15d94cb66af7a4fcab
Brian Tipton <brian.p.tipton@gmail.com>
2021-10-12 18:07:08 -0500

    remove extra $ from string

--- src/LatexParser.jl
+++ src/LatexParser.jl
@@ -1,21 +1,31 @@
 module LatexParser
 using Markdown
 export tex_left, tex_center
+
+function fix_chars(equation::String)
+    return replace(equation, r"(\$|(\r\n|\n|\r))" => s"")
+end
+
+function bold_equation(equation::String)
+    equation = "\\boldsymbol{" * equation
+    equation =  equation * "}" 
+    return equation
+end
+
 function tex_left(equation::String, bold::Bool=false)
+    equation = fix_chars(equation)
     if bold
-        equation = "``\\boldsymbol{" * equation
-        equation =  equation * "}``" 
-        return Markdown.parse(equation)
+        equation = bold_equation(equation)
     end
     equation = "``" * equation
     equation =  equation * "``" 
     return Markdown.parse(equation)
 end
+
 function tex_center(equation::String, bold::Bool=false)
+    equation = fix_chars(equation)
     if bold
-        equation = "\$\\boldsymbol{" * equation
-        equation =  equation * "}\$" 
-        return Markdown.parse(equation)
+        equation = bold_equation(equation)
     end
     equation = "\$" * equation
     equation =  equation * "\$" 
